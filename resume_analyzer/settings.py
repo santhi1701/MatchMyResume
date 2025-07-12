94970c0209129a3c291f366f2d89abdc20594647
santhi1701 <22kq1a5487aids@gmail.com>
2025-07-12 10:46:37 +0530

    upload

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -120,13 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS =  [
-     BASE_DIR / "static",
-    #os.path.join(BASE_DIR, 'static'),
-]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
